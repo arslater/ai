9e063a6668363e4c4624576628aeb3ee88aaee3e
anita <slater.anita@gmail.com>
2018-09-30 17:10:52 -0400

    [hw0] Created UFC, returns optimal path for vertex list

--- hw1/algos.py
+++ hw1/algos.py
@@ -6,7 +6,7 @@
 	by Anita Slater
 
 """
-import numpy as np
+import queue as Q
 from data import Data
 
 test=Data()
@@ -16,6 +16,18 @@ def dfs_recursive(graph, node, goal, visited):
     visited.append(node)
     if node == goal:
         print("BOOM")
+        return visited
+    for n in graph[node]:
+        #print(graph[node],"->{",n,"}")
+        if n not in visited:
+            #print("{{",n,"}}->",end="")
+            dfs_recursive(graph,n,goal,visited)
+    return visited
+
+def dfs_recursive_matrix(graph, node, goal, visited):
+    #print("@",node,"@", end="")
+    visited.append(node)
+    if node == goal:
         return visited
     for n in graph[node]:
         #print(graph[node],"->{",n,"}")
@@ -41,8 +53,43 @@ def bfs_stack(graph,start,visited):
             visited.append(n)
             stack.extend(graph[n])
     return visited
-def ucs_vertex():
-    print("https://stackoverflow.com/questions/43354715/uniform-cost-search-in-python")
+
+def ucs_vertex(graph, root, goal):
+    pq=Q.PriorityQueue()
+    weight = 0
+    visited = []
+    path = []
+    parents = {root:None}
+
+    fringe=graph[root]
+    pq.put((weight,root))
+
+    while not pq.empty():
+       # print("**: ", pq)
+        dq_item=pq.get()
+        cur_node=dq_item[1]
+        cur_node_weight=dq_item[0]
+        #print(cur_node_weight)
+
+        if cur_node == goal:
+            path = [cur_node]
+            prev_node = cur_node
+
+            while prev_node != root:
+                parent = parents[prev_node]
+                path.append(parent)
+                prev_node = parent
+
+            path.reverse()
+            return path
+        else:
+            for edge,key in graph[cur_node].items():
+                child = edge
+
+                if child not in visited:
+                    visited.append(child)
+                    parents[child] = cur_node
+                    pq.put((weight+key,child))
 
 def vert_2_matrix_2(graph):
     entries=[]
@@ -63,9 +110,10 @@ def vert_2_matrix_2(graph):
         row=[]
         #print()
     return columns
-graph=test.test
+graph=test.g3_vertex
 
-print(dfs_recursive(graph,'S','G',[]))
-print(dfs_stack(graph,'S','G',[]))
-print(bfs_stack(graph,'S',[]))
+#print(dfs_recursive(graph,'S','G',[]))
+#print(dfs_stack(graph,'S','G',[]))
+#print(bfs_stack(graph,'S',[]))
+print(ucs_vertex(graph,'S','G'))
 
